00b99039b5fcf36e11e7b5d6bf2a6457de0de58e
Posypy <claveltheo.sd@gmail.com>
2023-01-08 16:22:22 +0100

    Wwise_UI

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_SFX_UI_MENUPAUSE = 3069816448U;
+        static const AkUniqueID PLAY_SFX_UI_MM_BACK = 4031081037U;
+        static const AkUniqueID PLAY_SFX_UI_MM_FADER = 1008742184U;
+        static const AkUniqueID PLAY_SFX_UI_MM_NAVIGATE = 2349659389U;
+        static const AkUniqueID PLAY_SFX_UI_MM_PLAYBUTTON = 2787690146U;
+        static const AkUniqueID PLAY_SFX_UI_MM_VALIDATION = 1912011125U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
