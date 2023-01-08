0e997b1df5f380e2a0956ddcab700a5bc4294e42
Posypy <claveltheo.sd@gmail.com>
2023-01-08 17:40:52 +0100

    Revert "Merge branch 'main' of https://github.com/VessyQ/Tp_07_AdventureGame_Axel-Th-o"
    This reverts commit da4832b260092a4d04211576b9392f3a2c39a29c, reversing
    changes made to 3f0c8a706893b131686f9f1f331c2f8ac628659a.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,27 +13,18 @@ namespace AK
 {
     namespace EVENTS
     {
-<<<<<<< HEAD
-        static const AkUniqueID PLAY_PLACEHOLDER = 173327921U;
-        static const AkUniqueID STOP_PLACEHOLDER = 3637037023U;
-=======
         static const AkUniqueID PLAY_SFX_UI_MENUPAUSE = 3069816448U;
         static const AkUniqueID PLAY_SFX_UI_MM_BACK = 4031081037U;
         static const AkUniqueID PLAY_SFX_UI_MM_FADER = 1008742184U;
         static const AkUniqueID PLAY_SFX_UI_MM_NAVIGATE = 2349659389U;
         static const AkUniqueID PLAY_SFX_UI_MM_PLAYBUTTON = 2787690146U;
         static const AkUniqueID PLAY_SFX_UI_MM_VALIDATION = 1912011125U;
->>>>>>> b16d0f3532559a253e0f3c603f6d29e2abd18832
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-<<<<<<< HEAD
-        static const AkUniqueID PLACEHOLD = 4144675253U;
-=======
         static const AkUniqueID UI = 1551306167U;
->>>>>>> b16d0f3532559a253e0f3c603f6d29e2abd18832
     } // namespace BANKS
 
     namespace BUSSES
